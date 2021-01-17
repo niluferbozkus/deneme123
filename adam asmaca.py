876f784e4e45fedc9603bf1ce314a071395fca59
niluferbozkus <72705898+niluferbozkus@users.noreply.github.com>
2021-01-17 18:01:46 +0300

    Update adam asmaca.py

--- adam asmaca.py
+++ adam asmaca.py
@@ -3,7 +3,7 @@ print( "       ADAM ASMACA OYUNU" )
 kelime = input("Tahmin ettirmek istediğiniz kelimeyi girin: ").upper()
 
 
-resim = ["""
+adam = ["""
    +---+
    |   |
        |
@@ -57,29 +57,29 @@ resim = ["""
 
 adim = 0
 tahmin = []
-print(resim[adim])
+print(adam[adim])
 
 
 for i in kelime:
     tahmin.append("_")
 print(tahmin)
 
-while adim < 6 and "_" in tahmin:   # 7 resim listesininin uzunluğu 
+while adim < 6 and "_" in tahmin:   # 7 adam listesininin uzunluğu 
     harf = input("Tahmininizi girin: ").upper()
     
     if harf in kelime:
         for index, h in enumerate(kelime):
             if harf == h:
                 tahmin[index] = harf             
-                print(resim[adim])
+                print(adam[adim])
                 print(tahmin)        
     else:
         adim +=1
-        print(resim[adim])
+        print(adam[adim])
         print("{} harfi kelimede bulunmuyor.".format(harf))
         print(tahmin)
                 
-if adim < len(resim) and not "_" in tahmin:
+if adim < len(adam) and not "_" in tahmin:
     print("Kazandınız!")
 else:
     print("Kaybettiniz!")
